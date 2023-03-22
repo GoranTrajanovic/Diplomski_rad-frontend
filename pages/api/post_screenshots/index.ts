c43a5e7c1d5f24d6f44a8980db5e2f4b6eeb5dfb
Goran Trajanvoic <goran.trajanovic09@gmail.com>
2023-03-22 20:48:56 +0100

    Added generating screenshots for mobile/desktop and 3 browsers w/ Playwright.

--- pages/api/post_screenshots/index.ts
+++ pages/api/post_screenshots/index.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs, { mkdirSync } from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { chromium, devices } from "playwright";
+import { chromium, firefox, webkit, devices } from "playwright";
 
 type Data = {
 	url?: string;
@@ -11,21 +12,103 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const URLSforProcessing: string[] = req.body.url;
-	console.log(URLSforProcessing);
+	const URLForProcessing: string = req.body.url;
+	const URLWithoutHttps = URLForProcessing.substring(
+		URLForProcessing.indexOf("//") + 2
+	);
+	const rootURL = URLWithoutHttps.substring(0, URLWithoutHttps.indexOf("/"));
+	let URLSubpath = URLWithoutHttps.substring(URLWithoutHttps.indexOf("/") + 1);
+	URLSubpath = URLSubpath === "" ? "root" : URLSubpath;
 
-	const browser = await chromium.launch();
-	const contextPW = await browser.newContext();
-	// const context = await browser.newContext(devices["iPhone 11"]);
-	const page = await contextPW.newPage();
+	let today = new Date();
+	let date =
+		today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+	let time =
+		today.getHours() + "-" + today.getMinutes() + "-" + today.getSeconds();
+	let dateTimeFilename = date + "--" + time;
+
+	let dir = `app/screenshots/${dateTimeFilename}--${rootURL}`;
+
+	if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
 
 	try {
-		// await page.goto(URLSforProcessing[0]);
-		// await page.screenshot({ path: "newimg.png", fullPage: true });
+		let browser = await chromium.launch();
+		let context = await browser.newContext();
+		let page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_chromium_-_desktop.png`,
+			fullPage: true,
+		});
 
+		console.log("Processing screenshot: Chromium Desktop");
+
+		browser = await firefox.launch();
+		context = await browser.newContext();
+		page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_firefox_-_desktop.png`,
+			fullPage: true,
+		});
+
+		console.log("Processing screenshot: Firefox Desktop");
+
+		browser = await webkit.launch();
+		context = await browser.newContext();
+		page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_webkit_-_desktop.png`,
+			fullPage: true,
+		});
+
+		console.log("Processing screenshot: Webkit Desktop");
+
+		// mobile
+
+		browser = await chromium.launch();
+		context = await browser.newContext(devices["iPhone 11"]);
+		page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_chromium_-_mobile.png`,
+			fullPage: true,
+		});
+
+		console.log("Processing screenshot: Chromium Mobile");
+
+		// firefox mobile not supported
+
+		/* browser = await firefox.launch();
+		context = await browser.newContext(devices["iPhone 11"]);
+		page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_firefox_-_mobile.png`,
+			fullPage: true,
+		});
+
+		console.log("Processing screenshot: Firefox Mobile"); */
+
+		browser = await webkit.launch();
+		context = await browser.newContext(devices["iPhone 11"]);
+		page = await context.newPage();
+		await page.goto(URLForProcessing);
+		await page.screenshot({
+			path: `${dir}/${URLSubpath}_-_webkit_-_mobile.png`,
+			fullPage: true,
+		});
+
+		console.log("Processing screenshot: Webkit Mobile");
+
+		/* 
 		setTimeout(() => {
 			res.status(200).json({ url: req.body.url });
 		}, Math.floor(Math.random() * 5000) + 1000);
+		*/
+
+		res.status(200).json({ url: req.body.url });
 
 		/*
 		const links = await page.locator("a");
@@ -42,5 +125,6 @@ export default async function handler(
 		// takeScreenshotsForAllURLs(fullURLs);
 	} catch (err) {
 		console.log(err);
+		res.status(404).json({ errorMsg: "Error occured." });
 	}
 }
